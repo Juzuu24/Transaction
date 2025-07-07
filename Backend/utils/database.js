b84409dd294232f3ccf73a658a0ee55fa6877ddc
Ju Zuu Hlaing <juzuuhlaing_ju@cmu.ac.th>
2025-07-08 01:52:43 +0700

    backend of db port correction

--- Backend/utils/database.js
+++ Backend/utils/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Use environment variables
 const db = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "db",
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
